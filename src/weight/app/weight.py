7ab951278ce7f6015f127287d51b91e1e3ba0e84
Liel <liel.frank@gmail.com>
2025-07-26 19:23:25 +0300

    add health-check & added weight-db & sample files & test folder

---
+++ src/weight/app/weight.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from datetime import datetime 
+import sqlite3 
+
+app = Flask(__name__)
+
+
+# opens a connection to the database
+def get_db_connection():
+    conn = sqlite3.connect('weight.db')
+    conn.row_factory = sqlite3.Row # allows to access columns by name instead of index
+    return conn
+
+# health check
+@app.route("/health", methods=["GET"])
+def health_check():
+    try:
+        conn = get_db_connection() # connects to the database
+        conn.excute("SELECT 1;") # runs a query to check if the database is reachable
+        return "OK", 200
+    except :
+        return "Failure", 500
+    
